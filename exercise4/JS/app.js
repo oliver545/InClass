d2be0a9c6e3619f2a3c898e51861072a22e19b63
oliver545 <oliver.545@osu.edu>
2023-03-06 09:51:05 -0500

    March 6h

--- exercise4/JS/app.js
+++ exercise4/JS/app.js
@@ -1,32 +1,72 @@
-let melina = 50; //creating me own variable (global variable)
+let melina = 50; //creating my own variable (global variable)
+let A = 225;
+let B = 175;
+let C = 125;
+let D = 75; 
 
 function setup () {
     //happens only once
-    createCanvas(720, 400,); //add  WEBGL for 3D shapes
+    createCanvas(600, 700,); //add  WEBGL for 3D shapes
+    //myCanvas.parent('myContainer');
 }
 
 function draw() {
     //happens for every frame refresh (about 60/s)
     background(0,0,0);
-    fill(255, 0, 0);
-    ellipse(50,50,50,50);
-    //rotateX(frameCount * 0.01);
-    //rotateY(frameCount * 0.01);
-    //box(50);
-    // Draw a square at location (50, 100) with a side size of 155.
-    fill(255,127,0);
-    square(50, 100, 155, 20); //20 at the end is the rounded corners, radius of 20
-    fill(255,255,0);
-    triangle(300, 300, 350, 20, 200, 75);
-    fill(127,255,0);
-    square(500, 100, 70, 20);
-    fill(0,255,255);
-    arc(600, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
-    fill(127,0,255);
-    //using my made up variable
-    circle(width/4,height/4,melina); //melina is diameter
-    melina = melina +1;
-    if (melina > 200){
-        melina = 50;
+    noFill();
+    strokeWeight(4);
+    stroke(255,255,255);
+    ellipse(150,150,melina, melina);
+    ellipse(150,150,50,50);
+    melina = melina +1
+    if (melina > 200) {
+        melina = 50
     }
+    ellipse(350,150,50,50);
+    ellipse(450,350,50,50);
+    ellipse(250,350,50,50);
+    ellipse(150,550,melina,melina);
+    ellipse(450,350,melina,melina);
+    ellipse(150,550,50,50);
+    ellipse(350,550,50,50);
+    //spiral
+    strokeWeight(2);
+    ellipse(305,250,A,A);
+       /* A = A+1
+        if (A > 275) {
+            A = 255
+        }*/
+    ellipse(305,250,B,B);
+        B = B+1
+        if (B > A) {
+            B = 175
+        }
+    ellipse(305,250,C,C);
+        C = C+1
+        if (C > B) {
+            C = 125
+        }
+    ellipse(305,250,D,D);
+        D = D+1
+        if (D > C) {
+            D = 75
+        }
+    //mouse
+    strokeWeight(6);
+    stroke(25,50,75);
+    ellipse (mouseX, mouseY, 50,50);
+
+    helperCoordintates();
+}
+
+function helperCoordintates() {
+    fill(255);
+    text(
+        '('+ floor(mouseX)+
+        ','+ 
+        floor(mouseY)+
+        ')',
+        mouseX,
+        mouseY
+        );
 }
